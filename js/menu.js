87eef4efb63028d0f75e5b9b47275a6559c24a5f
sargoo <goon_aguea@hotmail.com>
2024-10-30 01:28:20 -0300

    se agrega menu superior responsive

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,4 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  const hamburger = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.nav-links');
-
-  hamburger.addEventListener('click', function() {
-    navLinks.classList.toggle('show');
-  });
-});
+function toggleMenu() {
+  const menu = document.querySelector('.navbar-menu');
+  menu.classList.toggle('active');
+}
